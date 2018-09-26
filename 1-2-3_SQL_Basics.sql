57bb0a0f24b4e7bc33c7a580a5d33cdc7d60e863
marmin <maryam.mgs@gmail.com>
2018-09-26 09:57:57 +0200

    Add files via upload

---
+++ 1-2-3_SQL_Basics.sql
@@ -0,0 +1,37 @@
+--What was the hottest day in our data set? Where was that?
+SELECT
+	MAX(MaxTemperatureF)
+FROM 
+	weather;
+-- You have not returned the date and the place
+
+--How many trips started at each station?
+SELECT
+	start_station,
+	COUNT(1)
+FROM
+	trips
+GROUP BY
+	start_station
+ORDER BY 2;
+
+--What's the shortest trip that happened?
+SELECT
+	*
+FROM 
+	trips
+WHERE 
+	duration = (
+				SELECT
+					min(duration)
+				FROM 
+					trips);
+
+--What is the average trip duration, by end station?
+SELECT
+	end_station,
+	AVG(duration)
+FROM
+	trips
+GROUP BY
+	end_station;
